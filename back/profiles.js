c19c11c8e64f8c4d271751e7310f87cb3258883f
guineveregilman <35609986+guineveregilman@users.noreply.github.com>
2018-05-10 02:58:55 -0400

    Actual Last Update

--- back/profiles.js
+++ back/profiles.js
@@ -73,7 +73,7 @@ router.post('/', function(request, response, next) {
       return next(new Error('Forbidden'));
     } else {
       // access control: user must be the author of the new profile
-      if (request.user.id !== request.body.user) return next(new Error('Forbidden'));
+      if (request.user.id !== request.body.user || request.user.name !== request.body.name) return next(new Error('Forbidden'));
         const profile = {
           author: {id: request.body.user, name: request.body.name},
           desc: request.body.desc,
